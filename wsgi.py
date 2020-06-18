722ebba7f14caae50cfce9d4c057fc4a0c3128ce
arkile <lunkans@ukr.net>
2020-06-18 19:07:17 +0300

    procfile little changes

---
+++ wsgi.py
@@ -0,0 +1,22 @@
+from flask import Flask
+# from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+# api = Api(app)
+#
+#
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+
+
+# api.add_resource(HelloWorld, '/')
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
